91971ac3fc7d5729736510460a6f38ad3a1566c0
Steven <steven.r987@gmail.com>
2021-11-23 14:39:37 -0700

    adding sidebar navigation & selectable menu

---
+++ src/components/SideBar.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { ProSidebar, Menu, MenuItem, SubMenu, SidebarHeader, SidebarFooter } from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { useState } from 'react';
+
+
+const SideBar = (props) => {
+    const [sidebarPosition, setSidebarPosition] = useState(false);
+
+    const onMinimizeSidebar = () => {
+        setSidebarPosition(true);
+    }
+
+    const onMaximizeSidebar = () => {
+        setSidebarPosition(false);
+    }
+
+    return (
+        <div className="sidebar">
+            <ProSidebar collapsed={sidebarPosition}>
+                <SidebarHeader className="side_bar_header">
+                    <MenuItem>Risk Management</MenuItem>
+                    {sidebarPosition ? 
+                        <MenuItem icon={<FontAwesomeIcon icon={faArrowRight} onClick={onMaximizeSidebar}/>}></MenuItem>
+                    :
+                        <MenuItem icon={<FontAwesomeIcon icon={faArrowLeft} onClick={onMinimizeSidebar}/>}></MenuItem>
+                    }
+                </SidebarHeader>
+                <Menu iconShape="square">
+                    <MenuItem icon={<FontAwesomeIcon icon={faGithub} />}>Dashboard</MenuItem>
+                    <SubMenu title="Components" icon={<FontAwesomeIcon icon={faGithub} />}>
+                        <MenuItem>Component 1</MenuItem>
+                        <MenuItem>Component 2</MenuItem>
+                    </SubMenu>
+                </Menu>
+                <SidebarFooter  className="bottom_link">
+                    <MenuItem icon={<FontAwesomeIcon icon={faGithub}/>}></MenuItem>
+                </SidebarFooter>
+            </ProSidebar>
+        </div>
+    )
+}
+
+export default SideBar;
